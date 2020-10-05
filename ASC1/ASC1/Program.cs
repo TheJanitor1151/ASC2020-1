bd63e4209fecd8b4e7f93a88e549fd54decfb2a9
TheJanitor1151 <45803183+TheJanitor1151@users.noreply.github.com>
2020-10-05 15:49:31 +0300

    Creat proiect Hello World

---
+++ ASC1/ASC1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World of C#!\n");
+            Console.ReadLine();
+            Console.WriteLine("\n");
+        }
+    }
+}
